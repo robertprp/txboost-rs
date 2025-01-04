1678f8ccf848a7798a4dd4eb39ba85d30ef17185
robertprp <robertforperez@gmail.com>
2025-01-04 19:35:43 +0100

    chore: prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,12 @@ pub mod bundle;
 pub mod jsonrpc;
 pub mod middleware;
 pub mod relay;
+
+pub mod prelude {
+    pub use super::{
+        bundle::*,
+        jsonrpc::*,
+        middleware::*,
+        relay::*,
+    };
+}
